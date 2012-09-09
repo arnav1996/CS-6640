25b9aadf3aad3a5349a9a1fd520da9e01761d2f0
Atul Rungta <atulrungta@155-99-187-154.uconnect.utah.edu>
2012-09-09 17:18:43 -0600

    histogram works

---
+++ histogram.m
@@ -0,0 +1,19 @@
+function h = histogram(I, n, min, max)
+    diff = max - min;
+    binSize = diff/n;
+    h = zeros(n+1, 1);
+    for i=1:size(I, 1)
+        for j=1:size(I, 2)
+            a = I(i, j);
+            bin = (a - min) * (1 / binSize);
+            if (bin == 0)
+                bin = 1;
+            end
+            h(bin) = (h(bin) + 1);
+        end
+    end
+    h = h/n;
+
+
+
+    
